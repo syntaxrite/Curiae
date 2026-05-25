0b61f86ded04945b3fd1ce5deb8426e888c905d4
Ritam Misra <rite.arm5967@gmail.com>
2026-05-25 21:49:00 +0530

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,27 @@
 // hello
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+const isPublicRoute = createRouteMatcher([
+  "/",
+  "/about(.*)",
+  "/rights(.*)",
+  "/sign-in(.*)",
+  "/sign-up(.*)",
+  "/api/webhooks/clerk"
+]);
+
+const isWebhookRoute = createRouteMatcher(["/api/webhooks/clerk"]);
+
+export default clerkMiddleware(async (auth, req) => {
+  if (isWebhookRoute(req)) {
+    return;
+  }
+
+  if (!isPublicRoute(req)) {
+    await auth.protect();
+  }
+});
+
+export const config = {
+  matcher: ["/((?!.*\\..*|_next).*)", "/(api|trpc)(.*)"]
+};
