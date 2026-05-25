4b18165250e5064747889f92569dcd6af7cdadd2
Ritam Misra <rite.arm5967@gmail.com>
2026-05-25 22:54:44 +0530

    Update page.tsx

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,1 +1,10 @@
 // hello
+import { SignIn } from "@clerk/nextjs";
+
+export default function SignInPage() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-background px-6">
+      <SignIn routing="path" path="/sign-in" signUpUrl="/sign-up" afterSignInUrl="/dashboard" />
+    </div>
+  );
+}
