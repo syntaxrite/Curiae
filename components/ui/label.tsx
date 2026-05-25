097206483aad366efc619f4c9bd662d3dc9a1d8d
Ritam Misra <rite.arm5967@gmail.com>
2026-05-25 23:06:54 +0530

    Add Label component with utility class names

--- components/ui/label.tsx
+++ components/ui/label.tsx
@@ -1,1 +1,7 @@
 // hello
+import * as React from "react";
+import { cn } from "@/lib/utils";
+
+export function Label({ className, ...props }: React.LabelHTMLAttributes<HTMLLabelElement>) {
+  return <label className={cn("text-sm font-medium text-text-primary", className)} {...props} />;
+}
