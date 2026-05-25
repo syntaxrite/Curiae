270d7caac22fdf642c3102c59fac1fe0889c55d8
Ritam Misra <rite.arm5967@gmail.com>
2026-05-26 01:15:43 +0530

    Update server.ts

--- convex/_generated/server.ts
+++ convex/_generated/server.ts
@@ -1,2 +1,17 @@
-// hello
-export { action, internalAction, internalMutation, internalQuery, mutation, query } from "convex/server";
+import {
+  actionGeneric,
+  httpActionGeneric,
+  mutationGeneric,
+  queryGeneric,
+  internalActionGeneric,
+  internalMutationGeneric,
+  internalQueryGeneric,
+} from "convex/server";
+
+export const action = actionGeneric;
+export const internalAction = internalActionGeneric;
+export const mutation = mutationGeneric;
+export const internalMutation = internalMutationGeneric;
+export const query = queryGeneric;
+export const internalQuery = internalQueryGeneric;
+export const httpAction = httpActionGeneric;
